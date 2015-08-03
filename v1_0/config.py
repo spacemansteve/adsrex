c9e6d205cb3798058ca25b086bb7b979eab775c5
Roman Chyla <roman.chyla@gmail.com>
2015-08-03 19:06:11 -0400

    Documentation: explained the config.py

--- v1_0/config.py
+++ v1_0/config.py
@@ -1,3 +1,12 @@
+
+# The main endpoint including the version, ie. https://api.adsabs.harvard.edu/v1 
 API_URL = 'https://api.adsabs.harvard.edu/v1'
+
+# OAuth access token for a dedicated user account; we have to create this account
+# and give it the normal oauth access rights (and occassionally reset it), for now
+# you can replace it with your own
 AUTHENTICATED_USER_ACCESS_TOKEN = ''
+
+
+# Access token given to any 'anonymous' bumblebee user
 BUMBLEBEE_ANONYMOUS_USER_ACCESS_TOKEN = ''
